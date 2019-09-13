fafc34839da5b1b0924ccd31f802081261bc6a6d
Allyssa Schnell <jschnell1969@gmail.com>
2019-09-13 10:25:51 -0400

    Added name

--- Intro to R.R
+++ Intro to R.R
@@ -1,7 +1,8 @@
 # R script to accopany Intro to R
 # Copy for RStudent 2019
 ######################################################
-
+#Written by Allyssa Schnell
+#Customized by Allyssa Schnell
 # You will see that you can put non-executable "comment" lines
 # in your script by putting "#" at the beginning of the lines.
 # 
